60575c5ed3ce9c8d9b9ed1a0232c407591a14015
karib shams <154616482+karibshams@users.noreply.github.com>
2026-09-26 15:35:47 +0600

    Enable override=True in load_dotenv to immediately reflect .env updates

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,7 @@ from dotenv import load_dotenv
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Load environment variables
-load_dotenv(BASE_DIR / '.env')
+load_dotenv(BASE_DIR / '.env', override=True)
 
 SECRET_KEY = os.getenv(
     'SECRET_KEY',
